1734c7ecf1368e3f6dd06bc266943fbd83f0cd9d
ElenaMalyugina <malyugina.el@yandex.ru>
2026-03-06 16:41:50 +0300

    исправлен конфип для публикации на гитхаб пейдж

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,21 @@ export default defineConfig({
   publicDir: 'static',
   server: {
     port: 3000,
-  }, 
+  },
   preview: {
     port: 3000
   },
   css: {
     postcss: './postcss.config.js',
   },
-  
+  build: {
+    outDir: 'dist',
+    assetsDir: '',
+    rollupOptions: {
+      output: {
+        assetFileNames: '[name][extname]'
+      }
+    }
+  }
+
 });
