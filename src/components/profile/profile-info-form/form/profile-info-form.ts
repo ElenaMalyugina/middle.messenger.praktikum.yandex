8e6684f880828cb3956842bf32cb207c0556c765
ElenaMalyugina <malyugina.el@yandex.ru>
2026-03-19 15:07:51 +0300

    убран хардкод из формы

--- src/components/profile/profile-info-form/form/profile-info-form.ts
+++ src/components/profile/profile-info-form/form/profile-info-form.ts
@@ -1,8 +1,33 @@
 import Form, { type FormProps } from "../../../../ui-units/form/form";
 import ProfileInfoFormTemplate from "./profile-info-form.hbs?raw";
 
-export default class ProfileInfoForm extends Form<FormProps> {
+interface UserInfo{
+    email: string;
+    login: string;
+    first_name: string;
+    second_name: string;
+    display_name: string;
+    phone: string;
+}
+
+const demoUser:UserInfo={
+    email: "pochta@yandex.ru",
+    login: "ivanivanov",
+    first_name: "Иван",
+    second_name: "Иванов",
+    display_name: "Иван123",
+    phone: "+7 909 967 30 30"
+}
+
+export default class ProfileInfoForm extends Form<UserInfo&FormProps> {
     static componentName = 'ProfileInfoForm';
     protected template = ProfileInfoFormTemplate;
+
+    protected componentDidMount(): void {
+        this.setProps({
+            ...this.props,
+            ...demoUser
+        })
+    }
 
 }
