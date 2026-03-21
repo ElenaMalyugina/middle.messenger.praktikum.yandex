c41e5474680017ddf9c7fb6a785bc1deeed06ba5
ElenaMalyugina <malyugina.el@yandex.ru>
2026-03-21 08:49:41 +0300

    модель смены пароля

--- src/components/profile/profile-change-password-form/form/profile-change-password-form.ts
+++ src/components/profile/profile-change-password-form/form/profile-change-password-form.ts
@@ -1,13 +1,29 @@
 import Form, { type FormProps } from "../../../../ui-units/form/form";
 import ProfileChangePasswordFormTemplate from "./profile-change-password-form.hbs?raw";
 
-interface ChangePasswordData{
-
+const mockData: ChangePassword ={
+    old_password: "",
+    new_password: "",
+    repeat_new_password: ""
 }
 
-export default class ProfileChangePasswordForm extends Form<FormProps> {
+interface ChangePassword{
+    old_password: string;
+    new_password: string;
+    repeat_new_password: string;
+}
+
+interface ProfileChangePasswordFormProps extends FormProps{
+    data: ChangePassword;
+}
+
+export default class ProfileChangePasswordForm extends Form<ProfileChangePasswordFormProps> {
     static componentName = 'ProfileChangePasswordForm';
     protected template = ProfileChangePasswordFormTemplate;
 
-
+    protected componentDidMount(): void {
+        this.setProps({
+            data: {...mockData}
+        })
+    }
 }
