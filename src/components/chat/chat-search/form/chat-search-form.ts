c6674e83f7a710e4b29e47f90f0c99823ec249db
ElenaMalyugina <malyugina.el@yandex.ru>
2026-03-21 07:41:01 +0300

    модель дял блока поиска

--- src/components/chat/chat-search/form/chat-search-form.ts
+++ src/components/chat/chat-search/form/chat-search-form.ts
@@ -1,9 +1,24 @@
-import Form from "../../../../ui-units/form/form";
+import Form, { type FormProps } from "../../../../ui-units/form/form";
 import ChatSearchFormTemplate from "./chat-search-form.hbs?raw";
 
-export default class ChatSearchForm extends Form{
+interface SearchData{
+    searchString: string;
+}
+
+interface ChatSearchFormProps extends FormProps{
+    data: SearchData;
+}
+
+export default class ChatSearchForm extends Form<ChatSearchFormProps>{
     static componentName = 'ChatSearchForm';
     protected template = ChatSearchFormTemplate;
 
     //Пока в ТЗ нет описания, как это должно работать
+    constructor(props: ChatSearchFormProps){
+        super(props);
+
+        this.props.data={
+            searchString: ""
+        }
+    }
 }
