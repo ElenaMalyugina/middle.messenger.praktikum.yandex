c4b18253e44103d3ebdae74ff21cc87ff58c6ce1
ElenaMalyugina <malyugina.el@yandex.ru>
2026-03-22 19:31:03 +0300

    фиксы

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,7 +1,7 @@
 import "./profile.css";
-import profileTemplate from "/src/pages/profile/profile.hbs?raw";
 import Block from "../../framework/Block";
 import { registerComponent } from "../../framework/RegisterComponent";
+import profileTemplate from "/src/pages/profile/profile.hbs?raw";
 import ProfileInfoBlock from "../../components/profile/profile-info/profile-info";
 import ProfileMenu from "../../components/profile/profile-menu/profile-menu";
 import ProfilePageLayout from "../../layouts/profile-page/profile-page-layout";
--- src/ui-units/button/button.ts
+++ src/ui-units/button/button.ts
@@ -27,5 +27,4 @@ export default class Button extends Block<ButtonProps>{
             }
         }
     }
-
 }
